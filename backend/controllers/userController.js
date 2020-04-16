7b3c34a6b961540db29a31342347f66c485e47d5
codecamp-official-thailand <codecamp.official.thailand@gmail.com>
2020-04-17 00:21:04 +0700

    refactor remove passport from register and login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,46 +1,77 @@
-const passport = require("passport");
 const passportObj = require("../config/passport/passport");
 const jwt = require("jsonwebtoken");
 const db = require("../models");
+const bcrypt = require("bcryptjs");
 
-const register = (req, res, next) => {
-  passport.authenticate("register", async (err, user, info) => {
-    if (err) {
-      console.log(err);
-    }
-    if (info) {
-      console.log(info);
-      res.status(400).send(info.message);
-    } else {
-      const name = req.body.name;
-      await user.update({ name: name });
-      await db.counter.create({ current: 0, user_id: user.id });
-      res.status(200).send({ message: "user created." });
-    }
-  })(req, res, next);
+const BCRYPT_SALT_ROUND = 12;
+
+const register = async (req, res) => {
+  const name = req.body.name;
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user = await db.user.findOne({ where: { username: username } });
+
+  if (user) {
+    console.log("Username already taken.");
+    res.status(400).send({ message: "Username already taken" });
+  } else {
+    const salt = bcrypt.genSaltSync(BCRYPT_SALT_ROUND);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    createNewUser({ username, hashedPassword, name });
+
+    res.status(200).send({ message: "user created." });
+  }
 };
 
-const login = (req, res, next) => {
-  passport.authenticate("login", async (err, user, info) => {
-    if (err) {
-      console.log(err);
+const createNewUser = async ({ username, hashedPassword, name }) => {
+  await db.user.create(
+    {
+      username: username,
+      password: hashedPassword,
+      name: name,
+      counter: {
+        current: 0,
+      },
+    },
+    {
+      include: [db.counter],
     }
-    if (info) {
-      console.log(info);
-      res.status.send(info.message);
+  );
+};
+
+const login = async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user = await db.user.findOne({ where: { username: username } });
+
+  if (!user) {
+    console.log("Username not found");
+    res.status(401).send({ message: "username or password is wrong" });
+  } else {
+    const isSuccess = bcrypt.compareSync(password, user.password);
+
+    if (!isSuccess) {
+      console.log("Password Do not match");
+      res.status(401).send({ message: "username or password is wrong" });
     } else {
-      const superSecretKey = passportObj.jwtOptions.secretOrKey;
       const payload = {
         id: user.id,
         name: user.name,
       };
-      const token = jwt.sign(payload, superSecretKey, { expiresIn: 60 });
+
+      const token = jwt.sign(payload, passportObj.jwtOptions.secretOrKey, {
+        expiresIn: 60,
+      });
+
       res.status(200).send({
         token: token,
         message: "User found & logged in",
       });
     }
-  })(req, res, next);
+  }
 };
 
 module.exports = {
